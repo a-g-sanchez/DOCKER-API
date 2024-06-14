0d4b1271dda4650123aa9ce466bdfc5bf4835afa
a-g-sanchez <aarongsanchez@google.com>
2024-06-14 10:45:35 -0500

    Added some json data and another endpoint to expose the data

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const morgan = require('morgan')
 const express = require('express')
 const app = express()
 
+const teams = require('./data.json')
+
 const port = 3001 || process.env.PORT
 
 app.use(morgan('dev'))
 
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
     res.send({msg: "Hello World"})
+})
+
+app.get('/teams', (req, res, next) => {
+    res.send({team: teams.teams})
 })
 
 app.listen(port, ()=> {
